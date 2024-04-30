594c1adc1de5f92a6c190a1d87e1371163fe03c0
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-04-30 16:54:51 +0530

    creating third variable and assign it a boolean

--- 1_variables_and_assignment.py
+++ 1_variables_and_assignment.py
@@ -4,6 +4,10 @@ print("x value:",x)
 #creating second variable and assign it a float
 float_no=10.5
 print("float valu",float_no)
+#creating third variable and assign it a boolean
+bool=True
+print("bool value",bool)
+
 
 
 
