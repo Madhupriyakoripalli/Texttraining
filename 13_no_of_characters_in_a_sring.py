16d4d9fc9125d8783a10d1f00b7c78e13fa1a882
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-03 14:29:20 +0530

    create a string from the user

---
+++ 13_no_of_characters_in_a_sring.py
@@ -0,0 +1,8 @@
+# create a string from the user
+user_string = input("Enter a string: ")
+# Calculate the number of characters in the string
+num_characters = len(user_string)
+# Print the user-entered string
+print("String entered by the user:", user_string)
+# Print the number of characters in the string
+print("Number of characters in the string:", num_characters)
