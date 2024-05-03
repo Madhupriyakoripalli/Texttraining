ca95b448d80ab9985ca36412d4cf9d39f559672e
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-03 12:33:52 +0530

    roman numeral equivalent

---
+++ 010_Roman_numeral_equivalent.py
@@ -0,0 +1,37 @@
+# creating Roman numeral equivalent
+import random
+
+number = random.randint(1, 15)
+
+if number == 1:
+    print("The Roman numeral equivalent of",number ,"is I")
+elif number == 2:
+    print("The Roman numeral equivalent of",number ,"is II")
+elif number == 3:
+    print("The Roman numeral equivalent of",number ,"is III")
+elif number == 4:
+    print("The Roman numeral equivalent of",number ,"is IV")
+elif number == 5:
+    print("The Roman numeral equivalent of",number ,"is V")
+elif number == 6:
+    print("The Roman numeral equivalent of",number ,"is VI")
+elif number == 7:
+    print("The Roman numeral equivalent of",number ,"is VII")
+elif number == 8:
+    print("The Roman numeral equivalent of",number ,"is VIII")
+elif number == 9:
+    print("The Roman numeral equivalent of",number ,"is IX")
+elif number == 10:
+    print("The Roman numeral equivalent of",number ,"is X")
+elif number == 11:
+    print("The Roman numeral equivalent of",number ,"is XI")
+elif number == 12:
+    print("The Roman numeral equivalent of",number ,"is XII")
+elif number == 13:
+    print("The Roman numeral equivalent of",number ,"is XIII")
+elif number == 14:
+    print("The Roman numeral equivalent of",number ,"is XIV")
+elif number == 15:
+    print("The Roman numeral equivalent of",number ,"is XV")
+else:
+    print("Invalid number generated")
