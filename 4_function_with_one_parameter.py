e33b4dd027e4f280c9d9cd169c4be4d7d83e3ba5
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-03 10:22:27 +0530

    Call name_printer() with the variable

--- 4_function_with_one_parameter.py
+++ 4_function_with_one_parameter.py
@@ -3,4 +3,5 @@ def name_printer(Madhupriya_koripalli):
     print(Madhupriya_koripalli) 
 #Create a variable called name and assign it user input()
 name = input("Enter your name") 
-
+#Call name_printer() with the variable
+name_printer(name)
