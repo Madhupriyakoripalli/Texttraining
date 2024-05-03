e015e2eae98c631933acaba37279f83779593b40
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-03 11:20:46 +0530

    calculate celsius to fahrenheit

---
+++ 6_Celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+# create and calculate Celsius to Fahrenheit
+Celsius = int(input("Enter celsius temperature:"))
+F = 1.8*Celsius+32
+def fahrenheit(Celsius):
+     return(F)
+print("The Fahrenheit equivalent of " +str(Celsius)+ "degrees Celsius is " +str(fahrenheit(Celsius)
