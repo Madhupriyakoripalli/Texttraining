b8677661f516e55649e7076405d4602446065677
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-03 13:10:20 +0530

    assign desending vaalues

--- 11_while_loop.py
+++ 11_while_loop.py
@@ -1,2 +1,9 @@
 #create a variable and assign it the integer 10
 num = 10
+#use a while loop to print numbers and the variable does not equal to 0
+while num != 0:
+     #print the value of the variable
+    print(num)
+    #reassign descending values to the variable 
+    num -= 1
+    
