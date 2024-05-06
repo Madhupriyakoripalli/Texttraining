4b0e856d1e2e756c7c3bb12f3db2e72079be2e1b
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-06 19:40:07 +0530

    get the string slice do it

--- 16_strings.py
+++ 16_strings.py
@@ -8,5 +8,8 @@ print(my_string[5:7])
 print(my_string[8:])
 # Print the slice "Just" from the variable
 print(my_string[:4])
+# Get the string slice "do it!" from the variable and concatenate it with the string "Don't ".
+resulting_string = "Don't " + my_string[5:]
+print(resulting_string)
 
 
