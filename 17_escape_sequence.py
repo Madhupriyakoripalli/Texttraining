3841049c3261eef134b3fc0a49eb70a347e0a8d9
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-06 19:50:12 +0530

    Use print() and type()

---
+++ 17_escape_sequence.py
@@ -0,0 +1,8 @@
+# Create a variable and assign it a float
+float = 3.14
+# Use print() and type() to print the data type of the variable
+print("Data type:", type(float))
+# Use str() on the variable
+print(str(float) + " is a float.")
+# print() the string "Hello, I'm [name], it's nice to meet you!" including quotes
+print("\"Hello, I'm [name], it's nice to meet you!\"")
