7fc7b41688ea067832c22f23fc5c909fcee14a45
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-07 14:39:21 +0530

    create upper case

--- 21_string_methods.py
+++ 21_string_methods.py
@@ -3,3 +3,5 @@ mixed_case = "A Song of Ice and Fire"
 print(mixed_case.isupper())
 # Check if mixed_case is all lower case
 print(mixed_case.islower())
+# Change all letters to upper case
+print(mixed_case.upper())
