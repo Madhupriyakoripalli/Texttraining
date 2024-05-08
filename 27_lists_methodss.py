5b8d8bde3e439419f7e1ab6038440f1fa8af48af
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 12:20:01 +0530

    add arctic fox

--- 27_lists_methodss.py
+++ 27_lists_methodss.py
@@ -4,5 +4,6 @@ arctic_animals = ["penguin", "elephant", "polar bear", "walrus", "tiger", "reind
 del arctic_animals[arctic_animals.index("tiger")]
 # Remove "elephant" from the list
 arctic_animals.remove("elephant")
-
+# Add "arctic fox" to the list
+arctic_animals.append("arctic fox")
 
