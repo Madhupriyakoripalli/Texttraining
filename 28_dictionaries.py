99f0402b0bdba15c90b930bc3f454676985ba0f6
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 15:06:30 +0530

    use in keyword

--- 28_dictionaries.py
+++ 28_dictionaries.py
@@ -8,3 +8,6 @@ dict = {
 }
 # Print and access the value held at the third key in the dictionary
 print("Value held at the third key:", dict["mahesh"])
+# Use the 'in' keyword to check if a key appears in the dictionary
+print("'madhu' appears in the dictionary:", 'madhu' in dict)
+
