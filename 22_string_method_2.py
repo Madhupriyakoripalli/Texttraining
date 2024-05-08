8cc475261b1785e26101aa1b575d791894511a5c
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 08:57:16 +0530

    padding with +

--- 22_string_method_2.py
+++ 22_string_method_2.py
@@ -1,5 +1,8 @@
 string = "North Dakota"
-# Right justify the_string with 17 spaces
+# Right justify the string with 17 spaces
 print(string.rjust(17))
-# Left justify the_string with 17 spaces, padding with "*"
+# Left justify the string with 17 spaces, padding with "*"
 print(string.ljust(17, "*"))
+# Center the string with 16 characters, padding with "+"
+center_plus =string.center(16, "+")
+print(center_plus)
