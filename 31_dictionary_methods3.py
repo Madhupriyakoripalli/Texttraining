9b3d9e3b86ba93a4e00469258960942d06030e00
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 21:31:34 +0530

    add the contents

--- 31_dictionary_methods3.py
+++ 31_dictionary_methods3.py
@@ -1,3 +1,5 @@
 # Paste the provided dictionaries into the .py file
 internet_celebrities = {"DrDisrespect": "YouTube", "ZLaner": "Facebook", "Ninja": "Mixer"}
 another_one = {"shroud": "Twitch"}
+# Use .update() to add the contents of another_one to internet_celebrities
+internet_celebrities.update(another_one)
