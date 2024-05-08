b753fd9f7c8d2051fd8df06d8998a57701915a40
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 09:15:40 +0530

    hold reverse string

--- 23_string_reverse.py
+++ 23_string_reverse.py
@@ -6,6 +6,7 @@ reversed_string = ""
 # Use a for loop to reverse the string
 for i in range(len(original_string) - 1, -1, -1):
     reversed_string += original_string[i]
-
+    
 # Print the reversed string
 print("Reversed string:", reversed_string)
+
