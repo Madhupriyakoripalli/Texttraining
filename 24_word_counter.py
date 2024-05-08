86365d58e290ba15ca65eb92465612431dc7335e
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 11:58:50 +0530

    count no of spaces in f string and add 1

--- 24_word_counter.py
+++ 24_word_counter.py
@@ -1,5 +1,6 @@
 def count_words(sentence):
     # Filter out characters besides numbers, letters, spaces, apostrophes, and hyphens
     filtered_sentence = ''.join(char if char.isalnum() or char in [' ', "'", "-"] else '' for char in sentence)
-   
+   # Count the number of spaces in the filtered string and add 1
+    return filtered_sentence.count(' ') + 1
 
