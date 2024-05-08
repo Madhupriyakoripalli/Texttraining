fe1d64d3a6c00aee226b34a40c8cc58f3f8b2634
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 15:28:50 +0530

    dictionary spanning multiple lines

---
+++ 29_dictionary_method.py
@@ -0,0 +1,9 @@
+# Create a dictionary spanning multiple lines
+music_dictionary = {
+    "Queen": "Bohemian Rhapsody",
+    "Bee Gees": "Stayin' Alive",
+    "U2": "One",
+    "Michael Jackson": "Billie Jean",
+    "The Beatles": "Hey Jude",
+    "Bob Dylan": "Like A Rolling Stone"
+}
