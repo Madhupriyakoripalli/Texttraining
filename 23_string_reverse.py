b382ed6867e9df14536a599bb99ae39c1c3b2efd
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-08 09:10:30 +0530

    take a input

---
+++ 23_string_reverse.py
@@ -0,0 +1,11 @@
+# Take a input
+original_string = input("Enter a string: ")
+# Initialize an empty string to hold the reversed string
+reversed_string = ""
+
+# Use a for loop to reverse the string
+for i in range(len(original_string) - 1, -1, -1):
+    reversed_string += original_string[i]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
