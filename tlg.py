b1c4a94e10c03c5f14a3be2e369e0fc1f9762577
madhupriya koripalli <madhupriya.k31@gmail.com>
2024-05-21 15:07:17 +0530

    the leading game

--- tlg.py
+++ tlg.py
@@ -1,11 +1,13 @@
-testcases = int(input())
+N = int(input())
+max_lead = 0
+leader = 0
 lead = 0
-for i in range(testcases):
-    player1,player2 = map(int,input().split())
-    if abs(player1 - player2) >= abs(lead):
-        lead = player1-player2
-        if lead>0:
-            w = 1
-        else:
-            w = 2
-print(w,abs(lead))
+for _ in range(N):
+    score1, score2 = map(int, input().split())
+    lead += score1 - score2
+    if abs(lead) > max_lead:
+        max_lead = abs(lead)
+        leader = 1 if lead > 0 else 2
+
+print(leader, max_lead)
+
